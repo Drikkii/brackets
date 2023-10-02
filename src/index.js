a813c8a7dfac53d7e16850cbb8620dafcfdc88d8
@Drikkii <chevpavel@gmail.com>
2023-10-02 19:49:46 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let chars = str.split(""),
+    stack = [],
+    open = [],
+    close = [],
+    closeIndex,
+    openIndex;
+
+  // Создаем массивы для открытых и закрытых скобок
+  bracketsConfig.forEach((config) => {
+    open.push(config[0]);
+    close.push(config[1]);
+  });
+
+  // Проходимся по строке, проверяя каждый ее символ.
+  for (let i = 0, len = chars.length; i < len; i++) {
+    openIndex = open.indexOf(chars[i]);
+    closeIndex = close.indexOf(chars[i]);
+
+    if (openIndex !== -1) {
+      // Нашли открывающую скобку. Помещаем ее в стек.
+      if (
+        openIndex === closeIndex &&
+        stack.length > 0 &&
+        stack[stack.length - 1] === openIndex
+      ) {
+        // Если это одинаковая скобка и она уже в стеке, закрываем ее
+        stack.pop();
+      } else {
+        stack.push(openIndex);
+      }
+      continue;
+    }
+
+    if (closeIndex !== -1) {
+      // Нашли закрывающую скобку. Проверяем ее соответствие открывающей .
+      openIndex = stack.pop();
+      if (closeIndex !== openIndex) {
+        return false;
+      }
+    }
+  }
+
+  // Проверяем дисбаланс открытых/закрытых скобок .
+  if (stack.length !== 0) {
+    return false;
+  }
+
+  return true;
+};
